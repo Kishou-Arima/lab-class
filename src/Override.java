88200cb7c833e741ab91937f55a0ff803e6c96f0
Kishou Arima <utkaarshsaha@gmail.com>
2021-12-02 12:44:54 +0530

    oi

---
+++ src/Override.java
@@ -0,0 +1,21 @@
+import javax.sql.rowset.spi.SyncResolver;
+
+class Automobiles
+{
+    void drive()
+    {
+        System.out.println("That's a car");
+    }
+}
+public class Override extends Automobiles
+{
+    void drive()
+    {
+        System.out.println("This is a car");
+    }
+    public static void main(String[] args) 
+    {
+        Override a=new Override();
+        a.drive();
+    }
+}
